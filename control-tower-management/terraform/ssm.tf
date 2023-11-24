179a8873b99983162eec874f89216ba822d8834e
Anders Porsbo <apor@tv2.dk>
2023-11-24 08:30:18 +0100

    added ssm param with timestamp to observe changes

--- control-tower-management/terraform/ssm.tf
+++ control-tower-management/terraform/ssm.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "SSMAccountID" {
 	type = "String"
 	value = "${data.aws_caller_identity.current.account_id}"
 }
+
+resource "aws_ssm_parameter" "ParamTimestamp" {
+	name = "/ParamTimestamp"
+	type = "String"
+	value = "2023-11-24 14:18"
+}
