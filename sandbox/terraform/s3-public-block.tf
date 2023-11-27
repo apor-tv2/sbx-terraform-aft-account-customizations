7ad097d511449a72ce6943b4d935605393753043
Anders Porsbo <apor@tv2.dk>
2023-11-27 04:45:36 +0100

    added public block

--- sandbox/terraform/s3-public-block.tf
+++ sandbox/terraform/s3-public-block.tf
@@ -3,7 +3,7 @@ resource "aws_s3_account_public_access_block" "global-block" {
 	block_public_policy = true
 }
 
- test by adding this to bucket policy
+# test by adding this to bucket policy
 #{
 #    "Version": "2012-10-17",
 #    "Statement": [
