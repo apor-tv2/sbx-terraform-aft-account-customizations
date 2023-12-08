5e14dba7e99fe4934f07f02f267ab30d302595da
Anders Porsbo <apor@tv2.dk>
2023-12-08 12:54:05 +0100

    changed name of bucket

--- control-tower-management/terraform/s3.tf
+++ control-tower-management/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "aft-mycontroltowermgmtaccount-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
