73b645396c19e7bee20313a64f04b8ef876ab9a9
Anders Porsbo <apor@tv2.dk>
2023-12-11 15:37:11 +0100

    deprecated acl on s3 bucket resource

--- control-tower-management/terraform/s3.tf
+++ control-tower-management/terraform/s3.tf
@@ -2,5 +2,4 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "aft-mycontroltowermgmtaccount-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
 }
