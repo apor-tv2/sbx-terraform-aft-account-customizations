f8fd338715ade0c5ffb30a79ed11e792fe0a766f
Anders Porsbo <apor@tv2.dk>
2023-12-18 11:00:47 +0100

    loosen SCP, because enrolling with AFT does not work

--- control-tower-management/terraform/scp.tf
+++ control-tower-management/terraform/scp.tf
@@ -1,14 +1,13 @@
 module "scp-sbx-labs" {
 	source = "./modules/org_scp/"
-	#targets = toset(["foo"],["bar"])
 	targets = toset([var.ou_targets.labs])
 	name = "scp-labs"
-	deny_root_account_access     = true
+	deny_root_account_access     = false
 	deny_password_policy_changes = true
 	deny_vpn_gateway_changes     = true
-	deny_vpc_changes             = true
+	deny_vpc_changes             = false
 	deny_config_changes          = true
 	# ^5
-	deny_cloudtrail_changes      = true
+	deny_cloudtrail_changes      = false
 	deny_leave_organization_and_change_billing = true
 }
