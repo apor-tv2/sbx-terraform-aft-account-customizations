bbdb96ff9f843c19afa2ed603d4a047b090bb7d9
Anders Porsbo <apor@tv2.dk>
2023-12-18 12:31:29 +0100

    changed scp to match, what I manually chanaged in organization

--- control-tower-management/terraform/scp-labs.tf
+++ control-tower-management/terraform/scp-labs.tf
@@ -4,10 +4,10 @@ module "scp-sbx-labs" {
 	name = "scp-labs"
 	deny_root_account_access     = false
 	deny_password_policy_changes = true
-	deny_vpn_gateway_changes     = true
+	deny_vpn_gateway_changes     = false
 	deny_vpc_changes             = false
 	deny_config_changes          = true
 	# ^5
-	deny_cloudtrail_changes      = false
-	deny_leave_organization_and_change_billing = true
+	deny_cloudtrail_changes      = true
+	deny_leave_organization_and_change_billing = false
 }
