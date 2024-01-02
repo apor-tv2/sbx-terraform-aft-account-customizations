305fe69e56600babb4ab6cee75ddab74d66e298b
Anders Porsbo <apor@tv2.dk>
2024-01-02 12:47:16 +0100

    adding config-rule via AFT

---
+++ sbxaudit/terraform/config_rules.tf
@@ -0,0 +1,48 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_config_rule.html
+resource "aws_config_config_rule" "r" {
+  name = "example"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
+  }
+
+  depends_on = [aws_config_configuration_recorder.foo]
+}
+
+resource "aws_config_configuration_recorder" "foo" {
+  name     = "example"
+  role_arn = aws_iam_role.r.arn
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "r" {
+  name               = "my-awsconfig-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "p" {
+  statement {
+    effect    = "Allow"
+    actions   = ["config:Put*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "p" {
+  name   = "my-awsconfig-policy"
+  role   = aws_iam_role.r.id
+  policy = data.aws_iam_policy_document.p.json
+}
