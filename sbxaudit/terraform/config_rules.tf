ef192bc7c85a0f14c81146cc4af2985480911ae9
Anders Porsbo <apor@tv2.dk>
2024-01-03 12:16:45 +0100

    renaming resources, and preparing for expantion to many config rules, renamed to foo again

--- sbxaudit/terraform/config_rules.tf
+++ sbxaudit/terraform/config_rules.tf
@@ -6,7 +6,8 @@ resource "aws_config_config_rule" "TV2Policy-S3_BUCKET_VERSIONING_ENABLED" {
     owner             = "AWS"
     source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
   }
-  depends_on = [aws_config_configuration_recorder.aws-controltower-BaselineConfigRecorder]
+  #depends_on = [aws_config_configuration_recorder.aws-controltower-BaselineConfigRecorder]
+  depends_on = [aws_config_configuration_recorder.foo]
 }
 # AWSControlTower-AWS-GR_AUDIT_BUCKET_PUBLIC_READ_PROHIBITED
 #acm-certificate-expiration-check
