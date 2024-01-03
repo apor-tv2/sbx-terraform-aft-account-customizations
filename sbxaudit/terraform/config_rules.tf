d4ff12e3db1a9af11c37f7a4810c25436c2967e4
Anders Porsbo <apor@tv2.dk>
2024-01-03 11:36:59 +0100

    renaming resources, and preparing for expantion to many config rules

--- sbxaudit/terraform/config_rules.tf
+++ sbxaudit/terraform/config_rules.tf
@@ -1,73 +1,13 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_config_rule.html
-resource "aws_config_config_rule" "r" {
-  name = "example"
+resource "aws_config_config_rule" "TV2Policy-S3_BUCKET_VERSIONING_ENABLED" {
+  name = "TV2Policy-S3_BUCKET_VERSIONING_ENABLED"
 
   source {
     owner             = "AWS"
     source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
   }
-
-#  depends_on = [aws_config_configuration_recorder.foo]
+  depends_on = [aws_config_configuration_recorder.aws-controltower-BaselineConfigRecorder]
 }
-
-import {
-  to = aws_config_configuration_recorder.foo
-  #id = "aws-controltower-GuardrailsComplianceAggregator"
-  id = "aws-controltower-BaselineConfigRecorder"
-}
-resource "aws_config_configuration_recorder" "foo" {
-  name     = "aws-controltower-BaselineConfigRecorder"
-  #role_arn = aws_iam_role.r.arn
-  #role_arn = "arn:aws:iam::996514947915:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
-  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
-  recording_group {
-    all_supported = true
-    include_global_resource_types = true # conflicts with resource_types
-
-#    resource_types = []
-#
-#    exclusion_by_resource_types {
-#      #resource_types = ["AWS::EC2::Instance"]
-#      resource_types = []
-#    }
-#
-#    recording_strategy {
-#      use_only = "ALL_SUPPORTED_RESOURCE_TYPES"
-#      #use_only = "EXCLUSION_BY_RESOURCE_TYPES"
-#    }
-  }
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_configuration_recorder#argument-reference
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["config.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "r" {
-  name               = "my-awsconfig-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "p" {
-  statement {
-    effect    = "Allow"
-    actions   = ["config:Put*"]
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "p" {
-  name   = "my-awsconfig-policy"
-  role   = aws_iam_role.r.id
-  policy = data.aws_iam_policy_document.p.json
-}
-
+# AWSControlTower-AWS-GR_AUDIT_BUCKET_PUBLIC_READ_PROHIBITED
+acm-certificate-expiration-check
+iam-password-policy
---
+++ sbxaudit/terraform/config_rules_common.tf
@@ -0,0 +1,59 @@
+import {
+  to = aws_config_configuration_recorder.aws-controltower-BaselineConfigRecorder
+  id = "aws-controltower-BaselineConfigRecorder"
+}
+#resource "aws_config_configuration_recorder" "foo" {
+resource "aws_config_configuration_recorder" "aws-controltower-BaselineConfigRecorder" {
+  name     = "aws-controltower-BaselineConfigRecorder"
+  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
+  recording_group {
+    all_supported = true
+    include_global_resource_types = true # conflicts with resource_types
+
+#    resource_types = []
+#
+#    exclusion_by_resource_types {
+#      #resource_types = ["AWS::EC2::Instance"]
+#      resource_types = []
+#    }
+#
+#    recording_strategy {
+#      use_only = "ALL_SUPPORTED_RESOURCE_TYPES"
+#      #use_only = "EXCLUSION_BY_RESOURCE_TYPES"
+#    }
+  }
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_configuration_recorder#argument-reference
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "r" {
+  name               = "my-awsconfig-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "p" {
+  statement {
+    effect    = "Allow"
+    actions   = ["config:Put*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "p" {
+  name   = "my-awsconfig-policy"
+  role   = aws_iam_role.r.id
+  policy = data.aws_iam_policy_document.p.json
+}
+
