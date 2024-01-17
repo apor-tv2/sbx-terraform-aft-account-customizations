dad44644bbf202d557f2c66ca09196672281e76b
Anders Porsbo <apor@tv2.dk>
2024-01-17 08:44:39 +0100

    changed remote repo to be in tv2 org on github

--- control-tower-management/terraform/scp-sandbox.tf
+++ control-tower-management/terraform/scp-sandbox.tf
@@ -1,7 +1,8 @@
 module "scp-sbx-sandbox" {
-	source = "https://git@github.com/apor-tv2/infrastructure-SCPs.git"
+	#source = "git::https://git@github.com/tv2/infrastructure-SCPs.git?ref=v1.0.1"
+	source = "git::https://git@github.com/tv2/infrastructure-SCPs.git"
+	#source = "https://git@github.com/apor-tv2/infrastructure-SCPs.git"
 	#source = "git::https://github.com/apor-tv2/infrastructure-SCPs.git"
-#	source = "git::https://github.com/tv2/infrastructure-SCPs.git?ref=v1.0.1"
 #source = "git@github.com:org/module"
 	#source = "./modules/org_scp/"
 	targets = toset([var.ou_targets.sandbox])
