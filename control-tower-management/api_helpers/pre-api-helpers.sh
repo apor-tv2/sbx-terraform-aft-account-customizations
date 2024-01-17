3720ad8c1e6e65004a8134de959ecd943275de3e
Anders Porsbo <apor@tv2.dk>
2024-01-17 09:49:09 +0100

    testing pre-api-helpers

--- control-tower-management/api_helpers/pre-api-helpers.sh
+++ control-tower-management/api_helpers/pre-api-helpers.sh
@@ -2,6 +2,8 @@
 
 echo "Executing Pre-API Helpers"
 echo "Test: Pre-API Helpers"
+echo "aws sts get-caller-identity"
+aws sts get-caller-identity
 
 # Configure Development SSH Key
 ssh_key_parameter=$(aws ssm get-parameter --name /aft/config/aft-ssh-key --with-decryption 2> /dev/null || echo "None")
