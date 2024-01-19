04db8c2825173cd09ba44b942258a91ab6669f16
Anders Porsbo <apor@tv2.dk>
2024-01-19 03:38:18 +0100

    removed resource for customizing scp-sandbox, to see if codestar connection works, without using it

--- control-tower-management/terraform/scp-sandbox.tf
+++ control-tower-management/terraform/scp-sandbox.tf
@@ -1,23 +1,23 @@
-module "scp-sbx-sandbox" {
-	#source = "git::https://git@github.com/tv2/infrastructure-SCPs.git?ref=v1.0.1"
-	#source = "git::https://git@github.com/tv2/infrastructure-SCPs.git"
-
-	# test with a codestar connection from control-tower-account
-	source = "git::https://git@github.com/apor-tv2/infrastructure-SCPs.git"
-
-	#source = "git::https://github.com/apor-tv2/infrastructure-SCPs.git"
-#source = "git@github.com:org/module"
-	#source = "./modules/org_scp/"
-	targets = toset([var.ou_targets.sandbox])
-	name = "scp-sandbox"
-	# Rule 1-5
-	deny_all_except_controltowerexecution = false
-	deny_root_account_access     = true
-	deny_password_policy_changes = true
-	deny_vpn_gateway_changes     = false
-	deny_vpc_changes             = false
-	# Rule 5-
-	deny_config_changes          = true
-	deny_cloudtrail_changes      = true
-	deny_leave_organization_and_change_billing = true
-}
+#module "scp-sbx-sandbox" {
+#	#source = "git::https://git@github.com/tv2/infrastructure-SCPs.git?ref=v1.0.1"
+#	#source = "git::https://git@github.com/tv2/infrastructure-SCPs.git"
+#
+#	# test with a codestar connection from control-tower-account
+#	source = "git::https://git@github.com/apor-tv2/infrastructure-SCPs.git"
+#
+#	#source = "git::https://github.com/apor-tv2/infrastructure-SCPs.git"
+##source = "git@github.com:org/module"
+#	#source = "./modules/org_scp/"
+#	targets = toset([var.ou_targets.sandbox])
+#	name = "scp-sandbox"
+#	# Rule 1-5
+#	deny_all_except_controltowerexecution = false
+#	deny_root_account_access     = true
+#	deny_password_policy_changes = true
+#	deny_vpn_gateway_changes     = false
+#	deny_vpc_changes             = false
+#	# Rule 5-
+#	deny_config_changes          = true
+#	deny_cloudtrail_changes      = true
+#	deny_leave_organization_and_change_billing = true
+#}
