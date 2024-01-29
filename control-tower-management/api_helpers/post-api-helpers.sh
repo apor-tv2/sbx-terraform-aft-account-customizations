2e4ff728c31f127252d34eafdd240dffd135bc65
Anders Porsbo <apor@tv2.dk>
2024-01-29 12:12:29 +0100

    git clone worked. terraform could checkout the moduel. about to test if we got the correct version

--- control-tower-management/api_helpers/post-api-helpers.sh
+++ control-tower-management/api_helpers/post-api-helpers.sh
@@ -5,3 +5,9 @@ echo "Executing Post-API Helpers"
 #find / -type d -path "*/modules/org_scp" 2>/dev/null
 #echo "pwd"
 #pwd
+echo cat $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/main.tf
+cat $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/main.tf
+
+echo ls $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/
+ls $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/
+
--- control-tower-management/terraform/scp-sandbox.tf
+++ control-tower-management/terraform/scp-sandbox.tf
@@ -33,20 +33,20 @@ module "scp-sbx-sandbox" {
 	# ../api_helpers/git-checkout-with-codestar.sh
 	#source = "./modules/infrastructure-SCPs"
 	source = "git::file:///tmp/infrastructure-SCPs?ref=v1.0.0"
-	targets = toset([var.ou_targets.sandbox])
-	name = "scp-sandbox"
-	# Rule 1-5
-	# THIS IS FOR SUSPSENDED ACCOUNTS
-	deny_all_except_controltowerexecution = false
-	# DISABLE ROOT, So MFA will not be needed anymore
-	deny_root_account_access     = true
-	# to not change password policy, it should be decided by tv2 organization policies only
-	deny_password_policy_changes = true
-	deny_vpn_gateway_changes     = false
-	deny_vpc_changes             = false
-	# Rule 5-
-	deny_config_changes          = true
-	deny_cloudtrail_changes      = true
-	# should not be possible to leave organization, so controltower cannot control account anymore
-	deny_leave_organization_and_change_billing = true
+#	targets = toset([var.ou_targets.sandbox])
+#	name = "scp-sandbox"
+#	# Rule 1-5
+#	# THIS IS FOR SUSPSENDED ACCOUNTS
+#	deny_all_except_controltowerexecution = false
+#	# DISABLE ROOT, So MFA will not be needed anymore
+#	deny_root_account_access     = true
+#	# to not change password policy, it should be decided by tv2 organization policies only
+#	deny_password_policy_changes = true
+#	deny_vpn_gateway_changes     = false
+#	deny_vpc_changes             = false
+#	# Rule 5-
+#	deny_config_changes          = true
+#	deny_cloudtrail_changes      = true
+#	# should not be possible to leave organization, so controltower cannot control account anymore
+#	deny_leave_organization_and_change_billing = true
 }
