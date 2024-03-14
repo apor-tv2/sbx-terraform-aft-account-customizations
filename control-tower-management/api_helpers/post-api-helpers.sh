e51707171a24524c1632eb94801b90907a87d3cc
Anders Porsbo <apor@tv2.dk>
2024-03-14 15:29:29 +0100

    removed code from post-api-helpers.sh

--- control-tower-management/api_helpers/post-api-helpers.sh
+++ control-tower-management/api_helpers/post-api-helpers.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo "Executing Post-API Helpers"
-#echo find / -type d -path "*/modules/org_scp"
-#find / -type d -path "*/modules/org_scp" 2>/dev/null
-#echo "pwd"
-#pwd
-echo cat $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/main.tf
-cat $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/main.tf
-
-echo ls $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/
-ls $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/
+#echo cat $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/main.tf
+#cat $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/main.tf
+#
+#echo ls $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/
+#ls $DEFAULT_PATH/$CUSTOMIZATION/terraform/.terraform/modules/scp-sbx-sandbox/
 
